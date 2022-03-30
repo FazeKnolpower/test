75fa85c26ade96c1e66e20f33df469673017f408
177783 <99659372+177783@users.noreply.github.com>
2022-03-30 10:07:21 +0200

    Add files via upload

---
+++ P3/Project.js
@@ -0,0 +1,9 @@
+function aanmelden() {
+                
+}
+document.getElementById("mijnknop1").onclick = aanmelden;
+
+function inloggen() {
+    
+}
+document.getElementById("mijnknop2").onclick = inloggen;
